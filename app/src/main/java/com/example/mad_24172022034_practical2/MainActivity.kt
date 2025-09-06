6ef2b8f3daa04fa08823bee1fc28cc1cf3dcf7d5
prit <pritpatel1179@gmail.com>
2025-09-06 19:05:37 +0530

    add snackbar

--- app/src/main/java/com/example/mad_24172022034_practical2/MainActivity.kt
+++ app/src/main/java/com/example/mad_24172022034_practical2/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.mad_24172022034_practical2
 
-import android.nfc.Tag
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -8,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     var TAG = "MainActivity"
@@ -27,34 +27,44 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "$meg", Toast.LENGTH_SHORT).show()
         Log.i(TAG,"Toast message show:$meg")
     }
+    fun showSnackbar(message: String) {
+        val rootView = findViewById<android.view.View>(R.id.main)
+        Snackbar.make(rootView, message, Snackbar.LENGTH_SHORT).show()
+    }
 
     override fun onStart() {
         super.onStart()
         showmeg("ON Start Call")
+        showSnackbar("On Start Call")
     }
 
     override fun onResume() {
         super.onResume()
         showmeg("On Resume Call")
+        showSnackbar("On Resume Call")
     }
 
     override fun onPause() {
         super.onPause()
         showmeg("On pause Call")
+        showSnackbar("On Pause Call")
     }
 
     override fun onStop() {
         super.onStop()
         showmeg("on Stop Call")
+        showSnackbar("On Stop Call")
     }
     override fun onRestart() {
         super.onRestart()
         showmeg("On Restart Call")
+        showSnackbar("On Restart Call")
     }
 
     override fun onDestroy() {
         super.onDestroy()
         showmeg("On Destroy Call")
+        showSnackbar("On Destroy Call")
     }
 
 }
